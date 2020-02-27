42ab9e22df32288bbb0cfa7f0d914bc3ad802197
ArcticWolfs <bdebrakeleer@gmail.com>
2020-02-27 15:38:04 +0100

    ajout de deux exercice

---
+++ VieEtMort/src/be/heh/www/Class1.java
@@ -0,0 +1,10 @@
+package be.heh.www;
+
+public class Class1
+{
+
+    public static void main(String[] args)
+    {
+        //Inutile
+    }
+}
---
+++ VieEtMort/src/be/heh/www/Class2.java
@@ -0,0 +1,6 @@
+package be.heh.www;
+
+public class Class2
+{
+
+}
